c31c04d99256e51c370cf63d6686541c33e25fe3
lihao25 <lihao25@meituan.com>
2021-07-26 15:07:06 +0800

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,8 @@ if [[ -d "${BUILD_DIR}" ]]; then
   rm -rf "${BUILD_DIR}"
 fi
 
+ln -fs /code/android-ndk-r21b "${V8_DIR}/third_party/android_ndk"
+
 cd "${V8_DIR}"
 
 if [[ ${MKSNAPSHOT_ONLY} = "1" ]]; then
@@ -12,6 +14,8 @@ if [[ ${MKSNAPSHOT_ONLY} = "1" ]]; then
 else
   gclient sync --deps=${PLATFORM} --reset --with_branch_head --revision ${V8_VERSION}
 fi
+
+python2 ./tools/node/fetch_deps.py $PWD
 
 cd "${ROOT_DIR}"
 scripts/patch.sh ${PLATFORM}
